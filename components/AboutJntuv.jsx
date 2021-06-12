bee23c888047c57d23908c208903d2d5d4c38540
royal-lobster <srujangs8@gmail.com>
2021-06-13 00:15:14 +0530

    :sparkles: added map
    added map

--- components/AboutJntuv.jsx
+++ components/AboutJntuv.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
 function AboutJntuv() {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: 300,
+    latitude: 18.151687350863902,
+    longitude: 83.3738706967883,
+    zoom: 16,
+  });
   return (
     <>
       <div className="aboutJntuv">
-        <h1 className="aboutJntuv__title">About JNTUK UCEV</h1>
+        <h1 className="aboutJntuv__title">
+          About <span>JNTUK UCEV</span>
+        </h1>
         <p className="aboutJntuv__text">
           The JNTUK University College of Engineering Vizianagaram has been
           established during September 2007 with five Under Graduate Engineering
@@ -17,9 +26,25 @@ function AboutJntuv() {
           separate University with this campus as constituent college and
           shifted to own campus at Dwarapudi, Vizianagaram during June,2009.
         </p>
+        <ReactMapGL
+          className="aboutJntuv__map"
+          mapboxApiAccessToken={process.env.MAPBOX_API_KEY}
+          {...viewport}
+          mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
+          onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        >
+          <Marker
+            latitude={18.151687350863902}
+            longitude={83.3738706967883}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <img src="/images/pin.png" width="30px" />
+          </Marker>
+        </ReactMapGL>
       </div>
 
-      <style jsx>
+      <style jsx global>
         {`
           .aboutJntuv {
             width: min(900px, 100%);
@@ -32,6 +57,20 @@ function AboutJntuv() {
           .aboutJntuv__text {
             color: #333;
             font-size: 18px;
+          }
+          .aboutJntuv__map {
+            width: 100%;
+            margin-top: 30px;
+          }
+          @media screen and (max-width: 700px) {
+            .aboutJntuv {
+              padding: 20px;
+              margin-top: 20px;
+              text-align: center;
+            }
+            .aboutJntuv__map {
+              width: 80%;
+            }
           }
         `}
       </style>
